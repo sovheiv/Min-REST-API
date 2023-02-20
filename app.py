fd6cf2e77999c40d291f7644b7e7bbe70639524f
SeniorVolodymyr <55vladimir5@gmail.com>
2023-02-20 18:26:44 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, abort, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return {"success": True, "msg": "message"}
+
+
+@app.route("/get_ip")
+def get_ip():
+    return {"ip": request.remote_addr}
+
+
+@app.route("/abort")
+def abort_test():
+    abort(403)
